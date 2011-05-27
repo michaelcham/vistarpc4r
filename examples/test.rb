0a0491e5355816d4a9cd66ec67e63f18fc8b23c1
Mike Cham <mike@blenderhouse.com>
2011-05-27 15:39:41 -0400

    Version bump to 0.2.0

--- examples/test.rb
+++ examples/test.rb
@@ -1,20 +1,20 @@
-require 'rpc_broker_connection'
+require './../lib/vistarpc4r/rpc_response'
+require './../lib/vistarpc4r/vista_rpc'
+require './../lib/vistarpc4r/rpc_broker_connection'
 
 #broker = RPCBrokerConnection.new('192.168.1.20', 9270, 'sys.admin', 'vista!123')
-broker = RPCBrokerConnection.new('openvista.medsphere.org', 9201, 'PU1234', 'PU1234!!')
+broker = VistaRPC4r::RPCBrokerConnection.new('openvista.medsphere.org', 9201, 'PU1234', 'PU1234!!')
 
 broker.connect
 broker.setContext('OR CPRS GUI CHART')
 
-wardsrpc = VistaRPC.new("ORQPT WARDS", RPCResponse::ARRAY)
-wardsresponse = broker.execute(wardsrpc)
-wardsresponse.value.each do |ward|
+patient_ien = "4"
+wardsarray = broker.call_a("ORQPT WARDS")
+wardsarray.each do |ward|
   a = ward.split("^")
   puts "Ward:" + a[1]
-  wardrpc = VistaRPC.new("ORQPT WARD PATIENTS", RPCResponse::ARRAY)
-  wardrpc.params[0]=a[0] #ward ien
-  wardresponse = broker.execute(wardrpc)
-  wardresponse.value.each do |patient|
+  wardarray = broker.call_a("ORQPT WARD PATIENTS", [a[0]])  # ward ien
+  wardarray.each do |patient|
     b = patient.split("^")
     puts b[0] + ":" + b[1]
   end
@@ -22,63 +22,49 @@ end
 
 # Problem list
 puts "Problem list-------------------------------------"
-patientrpc = VistaRPC.new("ORQQPL LIST", RPCResponse::ARRAY)
-patientrpc.params[0]="4" #patient ien
-patientrpc.params[1]="A" #patient ien
-patientresponse = broker.execute(patientrpc)
-patientresponse.value.each do |d|
+patientarray = broker.call_a("ORQQPL LIST", [patient_ien, "A"])
+patientarray.each do |d|
   puts d
 end
 
 # Medications
 puts "Medications-------------------------------------"
-patientrpc = VistaRPC.new("ORWPS COVER", RPCResponse::ARRAY)
-patientrpc.params[0]="4" #patient ien
-patientresponse = broker.execute(patientrpc)
-patientresponse.value.each do |d|
+patientarray = broker.call_a("ORWPS COVER", [patient_ien])
+patientarray.each do |d|
   puts d
 end
 
 #Labs
 puts "Labs-------------------------------------"
-patientrpc = VistaRPC.new("ORWCV LAB", RPCResponse::ARRAY)
-patientrpc.params[0]="4" #patient ien
-patientresponse = broker.execute(patientrpc)
-patientresponse.value.each do |d|
+patientarray = broker.call_a("ORWCV LAB", [patient_ien])
+patientarray.each do |d|
   puts d
 end
 
 # Vitals
 puts "Vitals-------------------------------------"
-patientrpc = VistaRPC.new("ORQQVI VITALS", RPCResponse::ARRAY)
-patientrpc.params[0]="4" #patient ien
-patientresponse = broker.execute(patientrpc)
-patientresponse.value.each do |d|
+patientarray = broker.call_a("ORQQVI VITALS", [patient_ien])
+patientarray.each do |d|
   puts d
 end
 
 # Demo
 puts "Demographics-------------------------------------"
-patientrpc = VistaRPC.new("ORWPT PTINQ", RPCResponse::ARRAY)
-patientrpc.params[0]="4" #patient ien
-patientresponse = broker.execute(patientrpc)
-patientresponse.value.each do |d|
+patientarray = broker.call_a("ORWPT PTINQ", [patient_ien])
+patientarray.each do |d|
   puts d
 end
 
 #visits
 puts "Visits-------------------------------------"
-patientrpc = VistaRPC.new("ORWCV VST", RPCResponse::ARRAY)
-patientrpc.params[0]="4" #patient ien
-patientresponse = broker.execute(patientrpc)
-patientresponse.value.each do |d|
+patientarray = broker.call_a("ORWCV VST", [patient_ien])
+patientarray.each do |d|
   puts d
 end
 
 #providers
 puts "Providers---------------------------------"
-patientrpc = VistaRPC.new("ORQPT PROVIDERS", RPCResponse::ARRAY)
-patientresponse = broker.execute(patientrpc)
-patientresponse.value.each do |d|
+patientarray = broker.call_a("ORQPT PROVIDERS")
+patientarray.each do |d|
   puts d
 end
